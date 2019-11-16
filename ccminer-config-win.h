0d560b37155b8073d9ebf8758308e3f8912a9ec1
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-16 15:31:05 +1300

    Update ccminer-config-win.h

--- ccminer-config-win.h
+++ ccminer-config-win.h
@@ -165,7 +165,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "ccminer8.21r18-yescryptr-v4"
+#define PACKAGE_VERSION "8.21r18-yescryptr-v4"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
